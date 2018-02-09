0bd218aadb44105c35450422480c80b12638735c
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2018-02-09 15:08:01 +0900

    READMEの更新 Version情報を取得するためのルートを追加

---
+++ Version.go
@@ -0,0 +1,31 @@
+package utility
+
+import (
+	"strconv"
+)
+
+const versionMajorCode = 1
+const versionMinorCode = 0
+const versionRevisionCode = 0
+
+var version string
+
+func Version() string {
+	if len(version) == 0 {
+		version = `v` + strconv.Itoa(versionMajorCode) + `.` + strconv.Itoa(versionMinorCode) + `.` + strconv.Itoa(versionRevisionCode)
+	}
+
+	return version
+}
+
+func VersionMajorCode() int {
+	return versionMajorCode
+}
+
+func VersionMinorCode() int {
+	return versionMinorCode
+}
+
+func VersionRevisionCode() int {
+	return versionRevisionCode
+}
