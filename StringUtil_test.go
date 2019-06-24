20672ae0c8388fb2af39b7229dcc2c839d6fc1c1
Hisashi Tatsuguchi <thcomp0520@gmail.com>
2019-06-24 12:00:23 +0900

    文字列分割関数の追加

--- StringUtil_test.go
+++ StringUtil_test.go
@@ -1,6 +1,7 @@
 package utility
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -42,6 +43,30 @@ func TestStringUtil2(t *testing.T) {
 		result = ToUpperFirst(testCase[1])
 		if result != testCase[0] {
 			t.Error(`ToLowerFirst is failed: result=` + result + `, ` + testCase[1] + `->` + testCase[0])
+		}
+	}
+}
+
+func TestStringUtil3(t *testing.T) {
+	var testCaseArray []string = []string{
+		`col1 = 1 OR col3="ANDO"`,
+		`(col1 = 1 AND col2 = "col2 value") OR col3="ANDO"`,
+	}
+	var testCaseExpectArray [][][]rune = [][][]rune{
+		{[]rune(`col1 = 1 `), []rune(`OR`), []rune(` col3="ANDO"`)},
+		{[]rune(`(`), []rune(`col1 = 1 `), []rune(`AND`), []rune(` col2 = "col2 value"`), []rune(`)`), []rune(` `), []rune(`OR`), []rune(` col3="ANDO"`)},
+	}
+
+	for i, testCase := range testCaseArray {
+		ret := Split(testCase, []string{"AND", "OR", "(", ")"}, []rune{'"', '\''}, false)
+		if len(ret) != len(testCaseExpectArray[i]) {
+			t.Errorf("length not matched: %d vs %d", len(ret), len(testCaseExpectArray[i]))
+		} else {
+			for j := 0; j < len(ret); j++ {
+				if strings.Compare(string(ret[j]), string(testCaseExpectArray[i][j])) != 0 {
+					t.Errorf("not matched: %s vs %s", string(ret[j]), string(testCaseExpectArray[i][j]))
+				}
+			}
 		}
 	}
 }
