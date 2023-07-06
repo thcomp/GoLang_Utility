f02ed1b73ece6246b9bc67b33d943335b7efaaeb
Menione_H.Tatsuguchi <hisashi-tatsuguchi@menicon.co.jp>
2023-07-06 12:12:29 +0900

    InXXXメソッドを追加

--- TimeHelper.go
+++ TimeHelper.go
@@ -14,7 +14,7 @@ type TimeHelper struct {
 var sInsTimeHelper *TimeHelper
 
 func (ins *TimeHelper) DateInUTC(year int, month time.Month, day, hour, min, sec, nsec int) (ret time.Time, retErr error) {
-     ret = time.Date(year, month, day, hour, min, sec, nsec, time.UTC)
+    ret = time.Date(year, month, day, hour, min, sec, nsec, time.UTC)
     
 
     return
@@ -29,7 +29,7 @@ func DateInUTC(year int, month time.Month, day, hour, min, sec, nsec int) (ret t
 }
 
 func (ins *TimeHelper) NowInUTC() (ret time.Time, retErr error) {
-     ret = time.Now().UTC()
+    ret = time.Now().UTC()
     
 
     return
@@ -41,6 +41,21 @@ func NowInUTC() (ret time.Time, retErr error) {
     }
 
     return sInsTimeHelper.NowInUTC()
+}
+
+func (ins *TimeHelper) InUTC(srcTime time.Time) (ret time.Time, retErr error) {
+    ret = srcTime.UTC()
+    
+
+    return
+}
+
+func InUTC(srcTime time.Time) (ret time.Time, retErr error) {
+    if sInsTimeHelper == nil {
+        sInsTimeHelper = &TimeHelper{}
+    }
+
+    return sInsTimeHelper.InUTC(srcTime)
 }
 
 func (ins *TimeHelper) DateInAsiaTokyo(year int, month time.Month, day, hour, min, sec, nsec int) (ret time.Time, retErr error) {
@@ -89,6 +104,30 @@ func NowInAsiaTokyo() (ret time.Time, retErr error) {
     }
 
     return sInsTimeHelper.NowInAsiaTokyo()
+}
+
+func (ins *TimeHelper) InAsiaTokyo(srcTime time.Time) (ret time.Time, retErr error) {
+    if ins.tzLocationAsiaTokyo == nil {
+        if tempTz, loadErr := time.LoadLocation(TzTextAsiaTokyo); loadErr == nil {
+            ins.tzLocationAsiaTokyo = tempTz
+        } else {
+            retErr = loadErr
+        }
+    }
+
+    if ins.tzLocationAsiaTokyo != nil {
+        ret = srcTime.In(ins.tzLocationAsiaTokyo)
+    }
+
+    return
+}
+
+func InAsiaTokyo(srcTime time.Time) (ret time.Time, retErr error) {
+    if sInsTimeHelper == nil {
+        sInsTimeHelper = &TimeHelper{}
+    }
+
+    return sInsTimeHelper.InAsiaTokyo(srcTime)
 }
 
 func (ins *TimeHelper) DateInJST(year int, month time.Month, day, hour, min, sec, nsec int) (time.Time, error) {
--- TimeHelperBuilder/method.tpl
+++ TimeHelperBuilder/method.tpl
@@ -1,5 +1,5 @@
 func (ins *{{.Structure}}) DateIn{{.Region}}{{.City}}(year int, month time.Month, day, hour, min, sec, nsec int) (ret time.Time, retErr error) {
-    {{if eq .City ""}} ret = time.Date(year, month, day, hour, min, sec, nsec, time.UTC)
+    {{if eq .City ""}}ret = time.Date(year, month, day, hour, min, sec, nsec, time.UTC)
     {{ else }}if ins.tzLocation{{.Region}}{{.City}} == nil {
         if tempTz, loadErr := time.LoadLocation(TzText{{.Region}}{{.City}}); loadErr == nil {
             ins.tzLocation{{.Region}}{{.City}} = tempTz
@@ -24,7 +24,7 @@ func DateIn{{.Region}}{{.City}}(year int, month time.Month, day, hour, min, sec,
 }
 
 func (ins *{{.Structure}}) NowIn{{.Region}}{{.City}}() (ret time.Time, retErr error) {
-    {{if eq .City ""}} ret = time.Now().UTC()
+    {{if eq .City ""}}ret = time.Now().UTC()
     {{ else }}if ins.tzLocation{{.Region}}{{.City}} == nil {
         if tempTz, loadErr := time.LoadLocation(TzText{{.Region}}{{.City}}); loadErr == nil {
             ins.tzLocation{{.Region}}{{.City}} = tempTz
@@ -46,4 +46,29 @@ func NowIn{{.Region}}{{.City}}() (ret time.Time, retErr error) {
     }
 
     return sIns{{.Structure}}.NowIn{{.Region}}{{.City}}()
+}
+
+func (ins *{{.Structure}}) In{{.Region}}{{.City}}(srcTime time.Time) (ret time.Time, retErr error) {
+    {{if eq .City ""}}ret = srcTime.UTC()
+    {{ else }}if ins.tzLocation{{.Region}}{{.City}} == nil {
+        if tempTz, loadErr := time.LoadLocation(TzText{{.Region}}{{.City}}); loadErr == nil {
+            ins.tzLocation{{.Region}}{{.City}} = tempTz
+        } else {
+            retErr = loadErr
+        }
+    }
+
+    if ins.tzLocation{{.Region}}{{.City}} != nil {
+        ret = srcTime.In(ins.tzLocation{{.Region}}{{.City}})
+    }{{end}}
+
+    return
+}
+
+func In{{.Region}}{{.City}}(srcTime time.Time) (ret time.Time, retErr error) {
+    if sIns{{.Structure}} == nil {
+        sIns{{.Structure}} = &{{.Structure}}{}
+    }
+
+    return sIns{{.Structure}}.In{{.Region}}{{.City}}(srcTime)
 }
