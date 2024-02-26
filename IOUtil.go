eeba607d803158c5b32e42b41be6a60409cb1e5a
Menione_H.Tatsuguchi <hisashi-tatsuguchi@menicon.co.jp>
2024-02-27 08:44:13 +0900

    NopCloserの対象をseeker, writerまで拡大

--- IOUtil.go
+++ IOUtil.go
@@ -6,25 +6,73 @@ import (
 )
 
 type NopCloser struct {
-	reader io.Reader
+	target interface{}
 }
 
-func NewNopCloser(reader io.Reader) *NopCloser {
-	return &NopCloser{
-		reader: reader,
+func NewNopCloser(target interface{}) *NopCloser {
+	if _, assertionOK := target.(io.Reader); assertionOK {
+		return &NopCloser{
+			target: target,
+		}
+	} else if _, assertionOK := target.(io.Seeker); assertionOK {
+		return &NopCloser{
+			target: target,
+		}
+	} else if _, assertionOK := target.(io.Writer); assertionOK {
+		return &NopCloser{
+			target: target,
+		}
 	}
+
+	return &NopCloser{}
 }
 
-func (closer *NopCloser) Close() error {
-	closer.reader = nil
-	return nil
+func (closer *NopCloser) Close() (err error) {
+	if hiddenCloser, assertionOK := closer.target.(io.Closer); assertionOK {
+		err = hiddenCloser.Close()
+	}
+
+	closer.target = nil
+	return err
 }
 
 func (closer *NopCloser) Read(p []byte) (n int, err error) {
-	if closer.reader != nil {
-		n, err = closer.Read(p)
+	if closer.target != nil {
+		if reader, assertionOK := closer.target.(io.Reader); assertionOK {
+			n, err = reader.Read(p)
+		} else {
+			err = fmt.Errorf("target not io.Reader")
+		}
 	} else {
-		err = fmt.Errorf("reader not opened")
+		err = fmt.Errorf("target not opened")
+	}
+
+	return
+}
+
+func (closer *NopCloser) Seek(offset int64, whence int) (n int64, err error) {
+	if closer.target != nil {
+		if seeker, assertionOK := closer.target.(io.Seeker); assertionOK {
+			n, err = seeker.Seek(offset, whence)
+		} else {
+			err = fmt.Errorf("fail to assert to io.Seeker")
+		}
+	} else {
+		err = fmt.Errorf("target not opened")
+	}
+
+	return
+}
+
+func (closer *NopCloser) Write(p []byte) (n int, err error) {
+	if closer.target != nil {
+		if writer, assertionOK := closer.target.(io.Writer); assertionOK {
+			n, err = writer.Write(p)
+		} else {
+			err = fmt.Errorf("fail to assert to io.Writer")
+		}
+	} else {
+		err = fmt.Errorf("target not opened")
 	}
 
 	return
